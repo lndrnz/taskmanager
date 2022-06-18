460788cb9b775f57517fcc592b52d5441806a06b
Lander Nunez <landernunez@gmail.com>
2022-06-17 18:18:35 -0700

    Feature 6

--- tracker/urls.py
+++ tracker/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
+    path("", RedirectView.as_view(url="/projects/"), name="home"),
 ]
