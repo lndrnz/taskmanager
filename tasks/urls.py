9299182f6a5f5e927c298ab5feef34d67b070179
Lander Nunez <landernunez@gmail.com>
2022-06-20 21:42:01 -0700

    feature 17

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from tasks.views import (
     CreateTaskView,
     TaskListView,
+    UpdateTaskView,
 )
 
 
 urlpatterns = [
     path("create/", CreateTaskView.as_view(), name="create_task"),
     path("mine/", TaskListView.as_view(), name="show_my_tasks"),
+    path("<int:pk>/complete/", UpdateTaskView.as_view(), name="complete_task"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -24,3 +24,11 @@ class TaskListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Task.objects.filter(assignee=self.request.user)
+
+
+class UpdateTaskView(LoginRequiredMixin, UpdateView):
+    model = Task
+    fields = ["is_completed"]
+
+    def get_success_url(self):
+        return reverse_lazy("show_my_tasks", args=[self.object.id])
